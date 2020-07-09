7a691b2bb86330256ad76f3d62fe3674b8ff4280
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-07-08 21:29:37 -0600

    Add test for finding all pets.

--- spec/models/favorite_spec.rb
+++ spec/models/favorite_spec.rb
@@ -8,6 +8,7 @@ RSpec.describe Favorite do
     @shelter_2 = create(:shelter)
     @pet_1 = create(:pet, name: "persy", age: 8, sex: "male", shelter: @shelter_1)
     @pet_2 = create(:pet, name: "piper", age: 12, sex: "female", shelter: @shelter_2)
+    @pet_3 = create(:pet, name: "holie", age: 4, sex: "female", shelter: @shelter_2)
   end
 
   it 'has can count pets' do
@@ -23,5 +24,10 @@ RSpec.describe Favorite do
     @favorite.add_pet(@pet_2)
     expect(@favorite.count).to eq(2)
     expect(@favorite.pet_data).to eq([@pet_1.id, @pet_2.id])
+  end
+
+  it 'can find all pets' do
+    @favorite.add_pet(@pet_3)
+    expect(@favorite.find_pets).to eq([@pet_1, @pet_2, @pet_3])
   end
 end
