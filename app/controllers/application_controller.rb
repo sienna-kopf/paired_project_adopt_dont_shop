81af83e5f9d6e1b3ed3c1177fc8f140ef861debf
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-07-08 19:33:10 -0600

    Add session to save the fav pet ids.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def favorites
+    session[:favorites] ||= []
+    binding.pry
+    Favorite.new(session[:favorites])
+  end
 end
