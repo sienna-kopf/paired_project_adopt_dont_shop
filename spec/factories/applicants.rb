dbfe688c5b9e3b0bf8e14becacc5708126b1c8f3
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-07-09 23:12:50 -0600

    Update the factories.

--- spec/factories/applicants.rb
+++ spec/factories/applicants.rb
@@ -1,11 +1,11 @@
 FactoryBot.define do
   factory :applicant do
-    name { Faker::Name.full_name }
+    name { Faker::Name.name }
     address { Faker::Address.full_address }
     city { Faker::Address.city }
     state { Faker::Address.state_abbr }
     zip { Faker::Address.zip }
-    phonenumber { "MyString" }
+    phonenumber { Faker::PhoneNumber.cell_phone }
     description { Faker::Lorem.paragraph(sentence_count: 3) }
   end
 end
