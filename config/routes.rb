7b493d7545ee9fe82aa55ad5f5a8259d1a491ae4
Sienna Kopf <princess.kopf@gmail.com>
2020-07-11 11:03:39 -0600

    Add functionality for user story 19

--- config/routes.rb
+++ config/routes.rb
@@ -42,4 +42,6 @@ Rails.application.routes.draw do
 
   get '/applications/new', to: 'applications#new'
   post '/applications', to: 'applications#create'
+
+  get '/applications/:id', to: 'applications#show'
 end
