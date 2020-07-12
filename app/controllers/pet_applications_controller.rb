185fb4b2373f63424c109f7fe734591f01fceb1b
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-07-12 00:59:34 -0600

    Story 22 complete. Approval.

--- app/controllers/pet_applications_controller.rb
+++ app/controllers/pet_applications_controller.rb
@@ -3,4 +3,19 @@ class PetApplicationsController < ApplicationController
     @pet = Pet.find(params[:pet_id])
     @applicants = @pet.applicants
   end
+
+  def update
+    pet = Pet.find(params[:pet_id])
+    params[:status] = "pending"
+    pet.update(pet_params)
+    pet.applicants = []
+    pet.applicants << Applicant.find(params[:applicant])
+
+    redirect_to "/pets/#{pet.id}"
+  end
+
+  private
+  def pet_params
+    params.permit(:status)
+  end
 end
