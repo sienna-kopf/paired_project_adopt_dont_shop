b627e25396b1e481940fa7ac8a8969003adc47aa
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-07-13 21:09:21 -0600

    Update methods to unappriobe pet + show application.

--- app/controllers/pet_applications_controller.rb
+++ app/controllers/pet_applications_controller.rb
@@ -6,40 +6,44 @@ class PetApplicationsController < ApplicationController
 
   def update
     pet_application = PetApplication.where(pet_id: params[:pet_id],applicant_id: params[:applicant])
-    pet_application.update(approval_status: 1)
+
     pet = Pet.find(params[:pet_id])
+    if PetApplication.where(pet_id: pet.id).pluck(:approval_status).include?(1)
+      flash[:error] = "No more applications can be approved for #{pet.name} at this time"
+      return redirect_to "/applications/#{params[:applicant]}"
+    end
+    pet_application.update(approval_status: 1)
+
     params[:status] = "pending"
     pet.update(pet_params)
-    # binding.pry
-    # applicant = Applicant.find(params[:applicant])
-    # if pet.status == 'adoptible' # "adoptable"
-    #   params[:status] = "pending"
-    #   if params[:approve_all]
-    #     applicant.pets.each do |pet|
-    #       add_pet(pet, applicant)
-    #     end
-    #     return redirect_to "/pets"
-    #   end
-    #   add_pet(pet, applicant)
-    #
-    #   redirect_to "/pets/#{pet.id}"
-    # else
-    #   params[:status] = "adoptible"
-    #   # flash[:error] = "No more applications can be approved for #{pet.name} at this time"
-    #   # return redirect_to "/applications/#{applicant.id}"
-    #   add_pet(pet, applicant)
 
-      redirect_to "/applications/#{params[:applicant]}"
+    redirect_to "/pets/#{pet.id}"
+  end
+
+  def unapprove
+    pet_application = PetApplication.where(pet_id: params[:pet_id],applicant_id: params[:applicant])
+    pet = Pet.find(params[:pet_id].to_i)
+
+    pet_application.update(approval_status: 0)
+    pet.update(status: "adoptible")
+
+    redirect_to "/applications/#{params[:applicant]}"
+  end
+
+  def approve_all
+    applicant = Applicant.find(params[:applicant])
+    applicant.pets.each do |pet|
+      pet_application = PetApplication.where(pet_id: pet.id,applicant_id: params[:applicant])
+      if PetApplication.where(pet_id: pet.id).pluck(:approval_status).include?(0)
+        pet.update(status: "pending")
+        pet_application.update(approval_status: 1)
+      end
+    end
+    redirect_to "/applications/#{params[:applicant]}"
   end
 
   private
   def pet_params
     params.permit(:status)
-  end
-
-  def add_pet(pet,applicant)
-    pet.update(pet_params)
-    pet.applicants.delete(applicant)
-    pet.applicants << applicant
   end
 end
