5a7f97b53fe25cd8d576f6c97bbf96c03ce3c96f
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-07-15 13:54:17 -0600

    Fix favorite model test.

--- spec/models/favorite_spec.rb
+++ spec/models/favorite_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe Favorite do
     @pet_1 = create(:pet, name: "persy", age: 8, sex: "male", shelter: @shelter_1)
     @pet_2 = create(:pet, name: "piper", age: 12, sex: "female", shelter: @shelter_2)
     @pet_3 = create(:pet, name: "holie", age: 4, sex: "female", shelter: @shelter_2)
-    @pet_3 = create(:pet, name: "harry", age: 5, sex: "male", shelter: @shelter_2)
+    @pet_4 = create(:pet, name: "harry", age: 5, sex: "male", shelter: @shelter_2)
   end
 
   it 'has can count pets' do
@@ -27,16 +27,20 @@ RSpec.describe Favorite do
     expect(@favorite.pet_data).to eq([@pet_1.id, @pet_2.id])
   end
 
-  xit 'can remove from favorites' do
+  it 'can remove from favorites' do
+    @favorite.add_pet(@pet_1)
+    @favorite.add_pet(@pet_2)
     expect(@favorite.count).to eq(2)
     expect(@favorite.pet_data).to eq([@pet_1.id, @pet_2.id])
 
-    @favorite.remove_pet(@pet_1)
+    @favorite.remove_pet(@pet_1.id)
     expect(@favorite.count).to eq(1)
     expect(@favorite.pet_data).to eq([@pet_2.id])
   end
 
   it 'can find all pets' do
+    @favorite.add_pet(@pet_1)
+    @favorite.add_pet(@pet_2)
     @favorite.add_pet(@pet_3)
     expect(@favorite.find_pets).to eq([@pet_1, @pet_2, @pet_3])
   end
